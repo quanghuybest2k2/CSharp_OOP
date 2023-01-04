4e1b697e1bd7f6012692cec1bbe0692b6687a662
Doan Quang Huy <2015597@dlu.edu.vn>
2023-01-04 21:49:57 +0700

    them de, quan ly phan so

---
+++ Thuc_Hanh/QuanLyPhanSo/PhanSo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhanSo
+{
+    internal class PhanSo
+    {
+        public int tu;
+        public int mau;
+        // khoi tao mac dinh, new PhanSo() khong can tham so
+        public PhanSo()
+        {
+            this.mau = 0;
+        }
+        // khoi tao co tham so
+        public PhanSo(int t, int m)
+        {
+            this.tu = t;
+            this.mau = m;
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhập tử: ");
+            tu = int.Parse(Console.ReadLine());
+            Console.Write("Nhập mẫu: ");
+            mau = int.Parse(Console.ReadLine());
+        }
+        public void Xuat()
+        {
+            Console.WriteLine($"{tu}/{mau}");
+        }
+        public PhanSo Cong(PhanSo a, PhanSo b)
+        {
+            PhanSo kq = new PhanSo();
+            kq.tu = a.tu * b.mau + b.tu * a.mau;
+            kq.mau = a.mau * b.mau;
+            return kq;
+        }
+    }
+}
---
+++ Thuc_Hanh/QuanLyPhanSo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyPhanSo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //PhanSo ps = new PhanSo(6,7);
+            //ps.Xuat();
+            //ps.Nhap();
+            //ps.Xuat();
+            PhanSo a = new PhanSo(5,8);
+            PhanSo b = new PhanSo(9,7);
+            PhanSo ps = a.Cong(a, b);
+            ps.Xuat();
+            Console.ReadLine();
+        }
+    }
+}
